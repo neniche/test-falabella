4147dedd3f8821bcfc1719d81e536e47a466e3d4
Denisse Falcon <macdenisse@Denisses-MacBook-Air.local>
2019-10-07 16:29:42 -0300

    correcting language errors

--- features/BranchsInquery.feature
+++ features/BranchsInquery.feature
@@ -1,22 +1,23 @@
-Feature: En la pagina de Tottus Nataniel buscar la receta de la corona de Rollitos de canela
+Feature:  search for Tottus Nataniel branch in google, and validate ingredients in a recipe in Tottus web page.
 
-  Como QA quiero validar que la receta de la corona de Rollitos de canela tenga azucar morena y
-  canela en la pagina del Tottus
+  Like QA, I want to search for Tottus Nataniel branch in google, and after validate some
+  ingredients in cinnamon Rolls recipe in Tottus web page.
 
-  Scenario: buscar el sitio web de tottus Nataniel
-    Given veo la pagina de google
-    When tipeo el texto "tottus sucursales" en la barra de busqueda de google
-    Then presiono la tecla "enter" en la barra de busqueda de google
-    Then deberia ver la lista de sucursales de tottus
-    Then busco la sucursal "Tottus Nataniel" en el listado
-    Then selecciono el boton "Sitio web"
-    Then veo la pagina web del Tottus
-    Then busco la opccion "Recetas"
-    Then veo la pagina de "Recetas"
-    Then selecciono la dificultad "Alta"
-    Then busco la receta "Corona de rollitos de canela"
-    Then veo la receta de "Corona de rollitos de canela"
-    Then valido que tenga "azúcar rubia"
-    Then valido que la receta tenga "canela" 
-    
-    
+
+  Scenario: Looking for Tottus Nataniel and ingredients in Cinnamon Rolls recipe
+    Given I see google web page
+    When I type "tottus sucursales" text in google search bar
+    Then I press on "enter" key in google search bar
+    Then I see tottus branches list
+    Then I look for "Tottus Nataniel" branch in the list
+    Then I click on "Sitio web" boton
+    Then I see tottus web page
+    Then I click on "Recetas" opcion
+    Then I see "Recetas" web page
+    Then I click on "Alta" dificulty
+    Then I click on "Corona de rollitos de canela" recipe
+    Then I see "Corona de rollitos de canela" page
+    Then I validate that "azúcar rubia" ingredient is in the recipe
+    Then I also validate that "canela" ingredient is in the recipe
+
+
--- features/step_definitions/BranchsInquery.js
+++ features/step_definitions/BranchsInquery.js
@@ -1,89 +1,84 @@
 const { Given, When, Then, } = require('cucumber');
 const { ClientFunction, Selector } = require('testcafe');
 
-Given('veo la pagina de google', async function () {
+Given('I see google web page', async function () {
     await testController.navigateTo('https://google.com');
 });
 
-When('tipeo el texto {string} en la barra de busqueda de google', async function (text) {
+When('I type {string} text in google search bar', async function (text) {
     const input = Selector('.gLFyf').with({ boundTestRun: testController });
-    await this.addScreenshotToReport();
     await testController.typeText(input, text);
 });
 
-Then('presiono la tecla {string} en la barra de busqueda de google', async function (text) {
+Then('I press on {string} key in google search bar', async function (text) {
     await testController.pressKey(text);
 });
 
-Then('deberia ver la lista de sucursales de tottus', async function () {
+Then('I see tottus branches list', async function () {
     const searchLink = Selector('.i0vbXd').with({ boundTestRun: testController });
     await testController.expect(searchLink.innerText).eql('Más ubicaciones');
     await testController.click(searchLink);
 });
 
-Then('busco la sucursal {string} en el listado', async function (text) {
+Then('I look for {string} branch in the list', async function (text) {
     const branchesList = Selector('div.dbg0pd').with({ boundTestRun: testController });
     const branchLabel = branchesList.withText(text);
     await testController.click(branchLabel);
 });
 
-Then('selecciono el boton {string}', async function (text) {
+Then('I click on {string} boton', async function (text) {
     const branchModal = Selector('a.CL9Uqc.ab_button').with({ boundTestRun: testController });
     const branchWebsiteButton = branchModal.withText(text);
-
     await testController.click(branchWebsiteButton);
 });
 
-Then('veo la pagina web del Tottus', async function () {
+Then('I see tottus web page', async function () {
     const getLocation = ClientFunction(() => window.location.href).with({ boundTestRun: testController });
     await testController.expect(await getLocation()).contains('https://www.tottus.cl/tottus/');
+    await this.addScreenshotToReport();
+    //await testController.navigateTo('https://www.tottus.cl/tottus/');
 });
 
-Then('busco la opccion {string}', async function (text) {
+Then('I click on {string} opcion', async function (text) {
     const recipeBoton = Selector('#new_tottus_nav_cliente_recetas').with({ boundTestRun: testController });
     await testController.click(recipeBoton);
 });
 
-Then('veo la pagina de {string}', async function (text) {
-    await testController.navigateTo('http://www.tottusrecetas.cl/');
+Then('I see {string} web page', async function (text) {
+    const getLocation = ClientFunction(() => window.location.href).with({ boundTestRun: testController });
+    await testController.expect(await getLocation()).contains('http://www.tottusrecetas.cl/');
 });
 
-When('selecciono la dificultad {string}', async function (text) {
-    const dificultyOption = Selector('#tab_container_21')
-        .find('button.btnfilter')
-        .nth(3)
-        .with({ boundTestRun: testController });
-
-    await testController
-        .expect(dificultyOption.exists).ok()
-        .expect(dificultyOption.textContent).contains(text);
+Then('I click on {string} dificulty', async function (text) {
+    const dificultyOption = Selector('#tab_container_21').find('button.btnfilter').nth(3).
+        with({ boundTestRun: testController });
+    await testController.expect(dificultyOption.exists).ok().expect(dificultyOption.textContent).contains(text);
     await testController.click(dificultyOption);
 });
 
-Then('busco la receta {string}', async function (text) {
-    const recipeOption = Selector('#divRecipes')
-        .find('.card-desc h3').with({ boundTestRun: testController });
+Then('I click on {string} recipe', async function (text) {
+    const recipeOption = Selector('#divRecipes').find('.card-desc h3').with({ boundTestRun: testController });
     const recipeLabel = recipeOption
         .filter((node, idx) => {
             return node.textContent === text;
         }, { text });
-    await testController
-        .expect(recipeLabel.exists).ok();
+    await testController.expect(recipeLabel.exists).ok();
     await testController.click(recipeLabel);
 });
 
-Then('veo la receta de {string}', async function (text) {
-    const getLocation = ClientFunction(() => window.location.href).with({ boundTestRun: testController });
+Then('I see {string} page', async function (text) {
+    const getLocation = ClientFunction(() => window.location.href)
+        .with({ boundTestRun: testController });
     await testController.expect(await getLocation()).contains('corona-de-rollitos-de-canela');
 });
 
-Then('valido que tenga {string}', async function (text) {
+Then('I validate that {string} ingredient is in the recipe', async function (text) {
     const ingredientList = Selector('.divRecipe').with({ boundTestRun: testController });
     const ingredientLabel = ingredientList.withText(text);
     await testController.expect(ingredientLabel.exists).ok();
 });
 
-Then('valido que la receta tenga {string}', async function (text) {
+Then('I also validate that {string} ingredient is in the recipe', async function (text) {
     const ingredientList = Selector('.divRecipe').with({ boundTestRun: testController });
     const ingredientLabel = ingredientList.withText(text);
     await testController.expect(ingredientLabel.exists).ok();
